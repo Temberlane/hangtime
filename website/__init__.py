069c487601c6c831678064905dbc0e65cfe91c1c
Temberlane <60454907+Temberlane@users.noreply.github.com>
2024-09-28 12:37:36 -0400

    created test header

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,10 @@ def create_app():
 
     app.config['SECRET_KEY'] = 'asdadsaddasdd'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')    
+
     return app
