c61bb02bbc627a63ab4762e14bc22f8e3b041d48
Temberlane <60454907+Temberlane@users.noreply.github.com>
2024-09-28 15:09:06 -0400

    added get and post methods

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -10,7 +10,7 @@ def login():
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template("sign_up.html")
 
