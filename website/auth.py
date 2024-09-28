4e97ac0a5bf61640020b69026c32f971a3564324
Temberlane <60454907+Temberlane@users.noreply.github.com>
2024-09-28 15:16:46 -0400

    added sign up fnunctionality

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -12,6 +12,23 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email must be greater than 3 characters. ", catagory='error')
+        elif len(firstName) < 2:
+            flash("First name must be greater than 1 characters", catagory='error')
+        elif password1 != password2:
+            flash("Passwords don\'t match.", catagory='error')
+        elif len(password1 < 7):
+            flash("Password must be atleast 7 characters.", catagory='error')
+        else:
+            flash('Account created!', catagory='success')
+            # add user to database
     return render_template("sign_up.html")
 
 @auth.route('/add-schedule')
